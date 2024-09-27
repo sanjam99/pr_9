360f9b5e1f2c32a6c271c1ad293b382a44aaaad0
sanket <sanketjambhulkar018@gmail.com>
2024-09-28 00:28:48 +0530

    this for deploy

---
+++ backend/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://manish:erp2019@cluster0-shard-00-00.oekpl.mongodb.net:27017,cluster0-shard-00-01.oekpl.mongodb.net:27017,cluster0-shard-00-02.oekpl.mongodb.net:27017/ERP?ssl=true&replicaSet=atlas-3civah-shard-0&authSource=admin&retryWrites=true&w=majority'
+  };
